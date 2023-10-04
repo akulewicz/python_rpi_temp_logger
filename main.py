49b194ff81c4a300001a9b51da3057bd297e5cb9
Arkadiusz Kulewicz <a.kulewicz@gmail.com>
2023-10-04 18:20:00 +0200

    Add database context manager and database setup

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+import sqlite3
 import w1thermsensor
+from database import Database
+
+def setup():
+    with Database(connection) as database:
+        database.cursor.execute('''
+                                CREATE TABLE temperatures
+                                (
+                                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                    temperature REAL NOT NULL,
+                                    measurement_time DATE NOT NULL
+                                )
+                    ''')
+
 
 sensor = w1thermsensor.W1ThermSensor()
 
 print(sensor.get_temperature())
+
+
+connection = sqlite3.connect('database.db')
+
